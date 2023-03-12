eb453ead32f3cad98bdc3f0925042963d38fe63a
max-soloshchanskiy <max.soloshchanskiy@kellogg.northwestern.edu>
2023-03-12 02:30:37 +0000

    hw4 without css

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,9 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+    if @current_user
+      @posts = Post.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
+    end
   end
 
   def new
